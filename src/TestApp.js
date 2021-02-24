db5597ab4c218f1825aeeff0fa8476ddd61d8be6
Juan Olvera <jp.oes@hotmail.com>
2021-02-24 17:56:59 -0600

    feat: display content for each role

--- src/TestApp.js
+++ src/TestApp.js
@@ -1,16 +1,20 @@
-import React from 'react'
-import LoginButton from './components/LoginButton'
-import LogoutButton from './components/LogoutButton'
-import TestProfile from './views/TestProfile'
-const TestApp = () => {
-    return (
-        <div>
-            <h1>Hi ther</h1>
-            <LoginButton/>
-            <LogoutButton/>
-            <TestProfile/>
-        </div>
-    )
-}
+import React from 'react';
+import LoginButton from './components/LoginButton';
+import LogoutButton from './components/LogoutButton';
+import TestProfile from './views/TestProfile';
+import { useAuth0 } from '@auth0/auth0-react';
 
-export default TestApp
+const TestApp = () => {
+  const { isAuthenticated } = useAuth0();
+
+  return (
+    <div>
+      <h1>Welcome</h1>
+          {isAuthenticated ? <LogoutButton /> : <LoginButton />}
+          {isAuthenticated ? <TestProfile /> : <div>You need to login or signup</div>}
+      
+    </div>
+  );
+};
+
+export default TestApp;
--- src/views/TestProfile.js
+++ src/views/TestProfile.js
@@ -1,23 +1,43 @@
-import React from "react";
-import { useAuth0 } from "@auth0/auth0-react";
+import React, { useState, useEffect } from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
 
 const TestProfile = () => {
   const { user, isAuthenticated, isLoading } = useAuth0();
+  const [color, setColor] = useState('white');
+  const [role, setRole] = useState(null);
 
+  useEffect(() => {
+    if (isAuthenticated && user) {
+      const role = user['http://localhost:3000/roles'][0];
+      if (role && role === 'Admin') {
+        setColor('green');
+        setRole("Admin")
+      }
+    }
+  }, [isAuthenticated, user]);
   if (isLoading) {
     return <div>Loading ...</div>;
   }
 
-  return (
-    isAuthenticated ? (
-          <div>
-              <pre>{ JSON.stringify(user,null,3)}</pre>
-        <img src={user.picture} alt={user.name} />
-        <h2>{user.name}</h2>
-        <p>{user.email}</p>
-      </div>
-    ): "Anonymous user"
-  ) ;
+  return isAuthenticated ? (
+    <div
+      style={{
+        width: '100%',
+        height: '100%',
+        backgroundColor: color,
+      }}
+    >
+      {
+        role && role==="Admin" ? <h1>A</h1> : <h1>B</h1>
+      }
+      <pre>{JSON.stringify(user, null, 3)}</pre>
+      <img src={user.picture} alt={user.name} />
+      <h2>{user.name}</h2>
+      <p>{user.email}</p>
+    </div>
+  ) : (
+    'Anonymous user'
+  );
 };
 
 export default TestProfile;
